f88b28557c4adfe9e2d5916481549e0d3b282769
jiwon kim <jiwonkim@jiwonui-MacBookPro.local>
2024-07-21 17:57:41 +0900

    세로높이 사이즈변경

--- app/bm-chat/components/BmchatContainer.tsx
+++ app/bm-chat/components/BmchatContainer.tsx
@@ -78,7 +78,8 @@ const BmchatContainer = () => {
     // flex justify-center : 좌우중간 items-center : 상하중간
     <div className="flex justify-center h-screen bg-gray-100"> {/* 전체 화면 중앙에 정렬 */}
       <div style={{ boxShadow: '0 0 #0000, 0 0 #0000, 0 1px 2px 0 rgb(0 0 0 / 0.05)' }}
-        className=" bg-white p-6 m-3 rounded-lg border border-[#e5e7eb] w-full max-w-[440px]  h-full max-h-[634px]">
+        className=" bg-white p-6 m-3 rounded-lg border border-[#e5e7eb] w-full max-w-[440px] h-[574px]">
+          {/* 높이는 하단 Chat Ontainer와 맞춰줘야함 */}
 
         {/* Loading Spinner */}
         {loading && (
@@ -94,7 +95,7 @@ const BmchatContainer = () => {
         </div>
 
         {/* Chat Container */}
-        <div className="overflow-y-auto pr-4 h-[474px] w-full">
+        <div className="overflow-y-auto pr-4 h-[404px] w-full">
           {messages.map((msg, index) => (
             <div key={index} className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
               <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
